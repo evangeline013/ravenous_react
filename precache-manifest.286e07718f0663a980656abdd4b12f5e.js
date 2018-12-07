d7fb7f115d17f982bd03f745044a6879f9f639be
Eva <evangeline013@gmail.com>
2018-12-07 13:16:30 -0500

    Updates

---
+++ precache-manifest.286e07718f0663a980656abdd4b12f5e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/ravenous_react/static/media/background_search_mobile.5d0905e6.jpg"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/ravenous_react/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "f06dc84771340356f932",
+    "url": "/ravenous_react/static/js/runtime~main.f06dc847.js"
+  },
+  {
+    "revision": "977b4483a32b837dc52b",
+    "url": "/ravenous_react/static/js/main.977b4483.chunk.js"
+  },
+  {
+    "revision": "e1ada48e35de0e3b8be4",
+    "url": "/ravenous_react/static/js/1.e1ada48e.chunk.js"
+  },
+  {
+    "revision": "977b4483a32b837dc52b",
+    "url": "/ravenous_react/static/css/main.79da287a.chunk.css"
+  },
+  {
+    "revision": "147699059ebbfd71fe2306b3dc9dee54",
+    "url": "/ravenous_react/index.html"
+  }
+];
